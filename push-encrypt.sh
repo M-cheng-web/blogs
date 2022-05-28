c6f956502791d315b06af7dd1bc052b79fdcd100
cxh <2604856589@qq.com>
2022-05-29 00:36:36 +0800

    test

--- push-encrypt.sh
+++ push-encrypt.sh
@@ -1,11 +1,11 @@
 sh SELF_ENCRYPT/index.sh on docs/encrypt
 
-sed -i "" "s/\encrypt\/every/encrypt\/every.md.encrypt/g" docs/.vuepress/config.js
+sed -i "" "s/\encrypt\/every/encrypt\/every.encrypt.md/g" docs/.vuepress/config.js
 
 sh deploy.sh
 
-git add .
-git commit -m 'feat'
-git push
+# git add .
+# git commit -m 'feat'
+# git push
 
-sh SELF_ENCRYPT/index.sh off docs/encrypt
+# sh SELF_ENCRYPT/index.sh off docs/encrypt
