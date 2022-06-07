787ed080788743b78051989431f699b46e8cf3ac
cxh <2604856589@qq.com>
2022-06-07 22:50:19 +0800

    feat

--- docs/arithmetic/core.md
+++ docs/arithmetic/core.md
@@ -1,2 +1,31 @@
 # 算法
 
+移动 0 , 可以用这种比较巧妙的方法,因为在添加或者删除数组元素时,会影响遍历执行时的动作,所以添加变量 a 来抵消这种影响
+``` js
+const run = (nums) => {
+  const len = nums.length
+  let a = 0
+  for (let index = 0; index < len; index++) {
+    if (nums[index - a] === 0) {
+      nums.splice(index - a, 1)
+      nums.push(0)
+      a++
+    }
+  }
+  return nums
+}
+```
+
+或者直接从最后一位开始遍历,这样就不会影响了
+``` js
+const run = (nums) => {
+  const len = nums.length
+  for (let index = nums.length; index >= 0; index--) {
+    if (nums[index] === 0) {
+      nums.splice(index, 1)
+      nums.push(0)
+    }
+  }
+  return nums
+}
+```
--- docs/arithmetic/index.js
+++ docs/arithmetic/index.js
@@ -1,18 +1,20 @@
-const run = (digits) => {
-  digits = digits.reverse()
-  let needAdd = false
-  for (let index = 0; index < digits.length; index++) {
-    if (digits[index] === 9) {
-      digits[index] = 0
-      needAdd = true
-    } else {
-      digits[index] += 1
-      return digits.reverse()
+const run = (matrix) => {
+  for (let x = 0; x < matrix.length; x++) {
+    for (let y = x + 1; y < matrix.length; y++) {
+      matrix[x][y] ^= matrix[y][x]
+      matrix[y][x] ^= matrix[x][y]
+      matrix[x][y] ^= matrix[y][x]
     }
+    matrix[x].reverse()
   }
-  if (needAdd) digits.push(1)
-  return digits.reverse()
+  console.log('matrix', matrix);
 }
 
-console.log(run([9, 9]));
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+// matrix = [[5, 1, 9, 11], [2, 4, 8, 10], [13, 3, 6, 7], [15, 14, 12, 16]]
 
+console.log(run(matrix));
+
+[[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+[[1, 4, 7], [2, 5, 6], [3, 8, 9]]
+[[1, 4, 7], [2, 5, 8], [3, 6, 9]] // 6 和 8 替换
