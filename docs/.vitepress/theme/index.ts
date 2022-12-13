de3c8e51eba43a6435884eea839059d7ddfe1a44
cxh <2604856589@qq.com>
2022-12-13 19:15:29 +0800

    commit log info

--- docs/.vitepress/theme/index.ts
+++
@@ -1,9 +0,0 @@
-import DefaultTheme from 'vitepress/theme'
-import Home from './components/home.vue'
-
-export default {
-  ...DefaultTheme,
-  enhanceApp({ app }) {
-    app.component('Home', Home)
-  }
-}
