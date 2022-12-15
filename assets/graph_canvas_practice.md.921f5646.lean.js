2688dc25677572727cf8a9ddf16b13bbb6a84bd5
cxh <2604856589@qq.com>
2022-12-15 18:51:25 +0800

    deploy

---
+++ assets/graph_canvas_practice.md.921f5646.lean.js
@@ -0,0 +1,41 @@
+import{_ as o,c as e,b as n,a as p,d as s,e as l,o as t,r as c}from"./app.4e511cb5.js";const I=JSON.parse('{"title":"\u5B9E\u8DF5\u4E00\u4E9B\u7B80\u5355\u6548\u679C","description":"","frontmatter":{},"headers":[{"level":2,"title":"\u7ED8\u5236\u5F62\u72B6","slug":"\u7ED8\u5236\u5F62\u72B6","link":"#\u7ED8\u5236\u5F62\u72B6","children":[{"level":3,"title":"\u76F4\u7EBF","slug":"\u76F4\u7EBF","link":"#\u76F4\u7EBF","children":[]},{"level":3,"title":"\u4E09\u89D2\u5F62","slug":"\u4E09\u89D2\u5F62","link":"#\u4E09\u89D2\u5F62","children":[]},{"level":3,"title":"\u77E9\u5F62","slug":"\u77E9\u5F62","link":"#\u77E9\u5F62","children":[]},{"level":3,"title":"\u5706\u5F27\u548C\u5706","slug":"\u5706\u5F27\u548C\u5706","link":"#\u5706\u5F27\u548C\u5706","children":[]},{"level":3,"title":"\u692D\u5706","slug":"\u692D\u5706","link":"#\u692D\u5706","children":[]},{"level":3,"title":"\u8D1D\u585E\u5C14\u66F2\u7EBF","slug":"\u8D1D\u585E\u5C14\u66F2\u7EBF","link":"#\u8D1D\u585E\u5C14\u66F2\u7EBF","children":[]},{"level":3,"title":"\u4E09\u6B21\u8D1D\u585E\u5C14\u66F2\u7EBF","slug":"\u4E09\u6B21\u8D1D\u585E\u5C14\u66F2\u7EBF","link":"#\u4E09\u6B21\u8D1D\u585E\u5C14\u66F2\u7EBF","children":[]}]},{"level":2,"title":"\u7ED8\u5236\u6837\u5F0F","slug":"\u7ED8\u5236\u6837\u5F0F","link":"#\u7ED8\u5236\u6837\u5F0F","children":[{"level":3,"title":"\u7EBF\u6761\u6837\u5F0F","slug":"\u7EBF\u6761\u6837\u5F0F","link":"#\u7EBF\u6761\u6837\u5F0F","children":[]},{"level":3,"title":"\u900F\u660E\u5EA6","slug":"\u900F\u660E\u5EA6","link":"#\u900F\u660E\u5EA6","children":[]},{"level":3,"title":"\u6E10\u53D8","slug":"\u6E10\u53D8","link":"#\u6E10\u53D8","children":[]},{"level":3,"title":"\u56FE\u6848\u6837\u5F0F","slug":"\u56FE\u6848\u6837\u5F0F","link":"#\u56FE\u6848\u6837\u5F0F","children":[]}]},{"level":2,"title":"\u7ED8\u5236\u6587\u672C","slug":"\u7ED8\u5236\u6587\u672C","link":"#\u7ED8\u5236\u6587\u672C","children":[{"level":3,"title":"\u6587\u672C\u6837\u5F0F","slug":"\u6587\u672C\u6837\u5F0F","link":"#\u6587\u672C\u6837\u5F0F","children":[]},{"level":3,"title":"\u9634\u5F71","slug":"\u9634\u5F71","link":"#\u9634\u5F71","children":[]}]},{"level":2,"title":"\u7ED8\u5236\u56FE\u7247","slug":"\u7ED8\u5236\u56FE\u7247","link":"#\u7ED8\u5236\u56FE\u7247","children":[{"level":3,"title":"\u7ED8\u5236","slug":"\u7ED8\u5236","link":"#\u7ED8\u5236","children":[]},{"level":3,"title":"\u7F29\u653E","slug":"\u7F29\u653E","link":"#\u7F29\u653E","children":[]},{"level":3,"title":"\u88C1\u526A","slug":"\u88C1\u526A","link":"#\u88C1\u526A","children":[]}]},{"level":2,"title":"\u53D8\u5F62","slug":"\u53D8\u5F62","link":"#\u53D8\u5F62","children":[{"level":3,"title":"\u4FDD\u5B58\u72B6\u6001","slug":"\u4FDD\u5B58\u72B6\u6001","link":"#\u4FDD\u5B58\u72B6\u6001","children":[]},{"level":3,"title":"\u5E73\u79FB & \u65CB\u8F6C & \u7F29\u5C0F","slug":"\u5E73\u79FB-\u65CB\u8F6C-\u7F29\u5C0F","link":"#\u5E73\u79FB-\u65CB\u8F6C-\u7F29\u5C0F","children":[]},{"level":3,"title":"transform setTransform resetTransform","slug":"transform-settransform-resettransform","link":"#transform-settransform-resettransform","children":[]}]},{"level":2,"title":"\u5408\u6210\u4E0E\u88C1\u526A","slug":"\u5408\u6210\u4E0E\u88C1\u526A","link":"#\u5408\u6210\u4E0E\u88C1\u526A","children":[{"level":3,"title":"\u5408\u6210","slug":"\u5408\u6210","link":"#\u5408\u6210","children":[]},{"level":3,"title":"\u88C1\u526A","slug":"\u88C1\u526A-1","link":"#\u88C1\u526A-1","children":[]}]},{"level":2,"title":"\u52A8\u753B","slug":"\u52A8\u753B","link":"#\u52A8\u753B","children":[{"level":3,"title":"demo","slug":"demo","link":"#demo","children":[]},{"level":3,"title":"\u901A\u8FC7demo\u660E\u786E\u7684\u95EE\u9898","slug":"\u901A\u8FC7demo\u660E\u786E\u7684\u95EE\u9898","link":"#\u901A\u8FC7demo\u660E\u786E\u7684\u95EE\u9898","children":[]}]},{"level":2,"title":"\u9AD8\u7EA7\u52A8\u753B","slug":"\u9AD8\u7EA7\u52A8\u753B","link":"#\u9AD8\u7EA7\u52A8\u753B","children":[{"level":3,"title":"\u901A\u8FC7demo\u660E\u786E\u7684\u95EE\u9898","slug":"\u901A\u8FC7demo\u660E\u786E\u7684\u95EE\u9898-1","link":"#\u901A\u8FC7demo\u660E\u786E\u7684\u95EE\u9898-1","children":[]}]}],"relativePath":"graph/canvas/practice.md","lastUpdated":1671027206000}'),r={name:"graph/canvas/practice.md"},F=p("",4),y=s("h3",{id:"\u4E09\u89D2\u5F62",tabindex:"-1"},[l("\u4E09\u89D2\u5F62 "),s("a",{class:"header-anchor",href:"#\u4E09\u89D2\u5F62","aria-hidden":"true"},"#")],-1),D=s("h3",{id:"\u77E9\u5F62",tabindex:"-1"},[l("\u77E9\u5F62 "),s("a",{class:"header-anchor",href:"#\u77E9\u5F62","aria-hidden":"true"},"#")],-1),C=s("ul",null,[s("li",null,"strokeRect(x, y, width, height) // \u7ED8\u5236\u4E00\u4E2A\u77E9\u5F62\u7684\u8FB9\u6846")],-1),A=s("ul",null,[s("li",null,"fillRect(x, y, width, height) // \u7ED8\u5236\u4E00\u4E2A\u586B\u5145\u7684\u77E9\u5F62")],-1),i=s("ul",null,[s("li",null,"clearRect(x, y, width, height) // \u6E05\u9664\u6307\u5B9A\u77E9\u5F62\u533A\u57DF\uFF0C\u8BA9\u6E05\u9664\u90E8\u5206\u5B8C\u5168\u900F\u660E")],-1),d=s("h3",{id:"\u5706\u5F27\u548C\u5706",tabindex:"-1"},[l("\u5706\u5F27\u548C\u5706 "),s("a",{class:"header-anchor",href:"#\u5706\u5F27\u548C\u5706","aria-hidden":"true"},"#")],-1),h=s("p",null,"arc(x, y, radius, startAngle, endAngle, anticlockwise)",-1),u=s("ul",null,[s("li",null,"x\u548CY\u4E3A\u5706\u5FC3\u7684\u5750\u6807"),s("li",null,"radius \u4E3A\u534A\u5F84"),s("li",null,"startAngle \u4E3A\u5706\u5F27\u6216\u5706\u7684\u5F00\u59CB\u4F4D\u7F6E"),s("li",null,"endAngle \u4E3A\u5706\u5F27\u6216\u5706\u7684\u7ED3\u675F\u4F4D\u7F6E"),s("li",null,"anticlockwise\u662F\u7ED8\u5236\u7684\u65B9\u5411\uFF08\u4E0D\u5199\u9ED8\u8BA4\u4E3Afalse\uFF0C\u4ECE\u987A\u65F6\u9488\u65B9\u5411\uFF09")],-1),g=s("h3",{id:"\u692D\u5706",tabindex:"-1"},[l("\u692D\u5706 "),s("a",{class:"header-anchor",href:"#\u692D\u5706","aria-hidden":"true"},"#")],-1),x=s("p",null,"ellipse(x, y, radiusX, radiusY, rotation, startAngle, endAngle, anticlockwise)",-1),m=s("ul",null,[s("li",null,"x\u3001y\uFF1A\u692D\u5706\u7684\u5706\u5FC3\u4F4D\u7F6E"),s("li",null,"radiusX\u3001radiusY\uFF1Ax\u8F74\u548Cy\u8F74\u7684\u534A\u5F84"),s("li",null,"rotation\uFF1A\u692D\u5706\u7684\u65CB\u8F6C\u89D2\u5EA6\uFF0C\u4EE5\u5F27\u5EA6\u8868\u793A"),s("li",null,"startAngle\uFF1A\u5F00\u59CB\u7ED8\u5236\u70B9"),s("li",null,"endAngle\uFF1A\u7ED3\u675F\u7ED8\u5236\u70B9"),s("li",null,"anticlockwise\uFF1A\u7ED8\u5236\u7684\u65B9\u5411\uFF08\u9ED8\u8BA4\u987A\u65F6\u9488\uFF09\uFF0C\u53EF\u9009\u53C2\u6570")],-1),v=p("",112);function b(f,E,k,w,q,_){const a=c("CanvasDemo");return t(),e("div",null,[F,n(a,{code:`
+let ctx = canvas.getContext('2d');
+ctx.moveTo(20, 20);
+ctx.lineTo(60, 60);
+ctx.stroke();
+`}),y,n(a,{code:`
+let ctx = canvas.getContext('2d');
+ctx.moveTo(0, 0);
+ctx.lineTo(200, 0);
+ctx.lineTo(0, 100);
+ctx.lineTo(0, 0);
+ctx.stroke();
+`}),D,C,n(a,{code:`
+let ctx = canvas.getContext('2d');
+ctx.strokeRect(0, 0, 100, 100);
+`}),A,n(a,{code:`
+let ctx = canvas.getContext('2d');
+ctx.fillRect(0, 0, 100, 100);
+`}),i,n(a,{height:"150",code:`
+let ctx = canvas.getContext('2d');
+ctx.fillRect(0, 0, 300, 130);
+ctx.clearRect(0,0, 100, 100);
+`}),d,h,u,n(a,{height:"300",width:"500",code:`
+let ctx = canvas.getContext('2d');
+ctx.beginPath(); // \u65B0\u5EFA\u4E00\u6761\u8DEF\u5F84\uFF0C\u4E5F\u5C31\u662F\u91CD\u5F00\u4E00\u4E2A\u7ED8\u753B
+ctx.arc(100, 100, 80, 20, Math.PI);
+ctx.closePath(); // \u95ED\u5408\u7ED8\u753B\uFF0C\u5982\u679C\u662F\u534A\u5706\u6DFB\u52A0\u4E86\u95ED\u5408\u4E5F\u4F1A\u628A\u53E3\u5C01\u6389
+ctx.stroke(); // \u63CF\u8FB9
+ctx.beginPath();
+ctx.arc(200, 200, 80, 20, Math.PI, true);
+<!-- ctx.closePath(); --> // \u8FD9\u91CC\u4E0D\u7528\u95ED\u5408\uFF0Canticlockwise=true\u4E5F\u80FD\u81EA\u52A8\u95ED\u5408
+ctx.stroke();
+ctx.beginPath();
+ctx.arc(400, 10, 80, 20, Math.PI);
+ctx.stroke();
+`}),g,x,m,n(a,{height:"300",code:`let ctx = canvas.getContext('2d');
+ctx.beginPath();
+ctx.ellipse(300, 300, 100, 100, 0, 0, Math.PI, true);
+ctx.closePath();
+ctx.stroke();
+`}),v])}const T=o(r,[["render",b]]);export{I as __pageData,T as default};
